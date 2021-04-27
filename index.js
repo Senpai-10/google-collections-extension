69e037f13e83277feceffcd989e65d0809488a37
senpai-10 <bmjfdrh@gmail.com>
2021-04-27 15:20:31 +0300

    check if pageNumber is undefined

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         chrome.tabs.query({active:true, currentWindow:true},function(tab){
             let url = tab[0].url
             const params = new URL(url).searchParams;
-            let pagenumber = params.get("pageNumber")
+            let pagenumber = params.get("pageNumber") != undefined ? params.get("pageNumber") : 2
 
             chrome.tabs.update({url: updateURLParameter(url, "pageNumber", parseInt(pagenumber)-1)})
         });
