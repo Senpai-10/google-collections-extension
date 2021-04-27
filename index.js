9a1614603f8f2bf463eb324e5289a045fa049778
senpai-10 <bmjfdrh@gmail.com>
2021-04-27 13:36:35 +0300

    get url

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    let previous = document.getElementById("previous").addEventListener("click", () => {
+        chrome.tabs.update({url:"https://www.google.com"});
+    })
+
+    let next = document.getElementById("next").addEventListener("click", () => {
+        chrome.tabs.query({active:true, currentWindow:true},function(tab){
+            let url = tab[0].url
+            alert(url);
+        });
+    })
 })
