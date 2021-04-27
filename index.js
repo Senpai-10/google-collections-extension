bf5437e518f9c804c62d8cf0648fe9ba57e97d7b
senpai-10 <bmjfdrh@gmail.com>
2021-04-27 14:45:25 +0300

    next event

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
+    function updateURLParameter(url, param, paramVal){
+        var newAdditionalURL = "";
+        var tempArray = url.split("?");
+        var baseURL = tempArray[0];
+        var additionalURL = tempArray[1];
+        var temp = "";
+        if (additionalURL) {
+            tempArray = additionalURL.split("&");
+            for (var i=0; i<tempArray.length; i++){
+                if(tempArray[i].split('=')[0] != param){
+                    newAdditionalURL += temp + tempArray[i];
+                    temp = "&";
+                }
+            }
+        }
+    
+        var rows_txt = temp + "" + param + "=" + paramVal;
+        return baseURL + "?" + newAdditionalURL + rows_txt;
+    }
+    
+    
     let previous = document.getElementById("previous").addEventListener("click", () => {
-        chrome.tabs.update({url:"https://www.google.com"});
+        chrome.tabs.update({url:"https://www.google.com"})
     })
 
     let next = document.getElementById("next").addEventListener("click", () => {
         chrome.tabs.query({active:true, currentWindow:true},function(tab){
             let url = tab[0].url
-            alert(url);
+            const params = new URL(url).searchParams;
+            let pagenumber = params.get("pageNumber")
+
+            chrome.tabs.update({url: updateURLParameter(url, "pageNumber", parseInt(pagenumber)+1)})
         });
     })
 })
